ba4770f51b43ee3e6d5c286bec756b836da42dfe
User <C:\Users\User\AppData\Roaming\The Bat!>
2022-02-07 21:41:14 +0300

    sem4 ex 59

--- sem4.py
+++ sem4.py
@@ -10,6 +10,7 @@ from functools import reduce
 from operator import eq
 import os
 import random
+from tkinter.messagebox import RETRY
 from unittest import result
 
 # 52.	Дана последовательность чисел. Получить список уникальных элементов заданной последовательности.
@@ -95,59 +96,175 @@ from unittest import result
 
 # 56.	Дан список чисел. Выделить среди них числа, удовлетворяющие условию: следующее больше предыдущего. 
 # Пример: [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3] или [1, 7] или [1, 6, 7] и т.д.
+# 1.Проходим цикл с каждым элементом. От элемента и до конца списка
+# 2.В результат пишем текущую позицию. 
+# 3.Если следующая позиция больше предыдущей A[i-1] < A[i], то вносим в результат. До конца списка.
+# 4.Идем дальше на пункт 3
+# 5.Если следующая позиция меньше (else от 3.), то  пропускаем и идем дальше на 3.. 
+# 6.Как прошли список, то начинаем с первого элемента, но сравнение уже не со вторым, а с третьм, четвертым ....
+# Итого три вложенности. А: проход по каждому элементу. В: Элемент из прошло цикла и следующий. С: Элемент из первого, второго цикла и до конца списка.
+
+# Вариант с рекурсией
+# def get_sub_list(item_index, init_lst, temp_list):
+#     result_list = []
+#     init_len = len(init_lst)
+#     for sub_item_index in range(item_index, init_len):
+#         if init_lst[sub_item_index] > temp_list[len(temp_list) - 1]:
+#             temp_list += [init_lst[sub_item_index]]
+#             result_list += [temp_list[:]]
+#         else:
+#             if init_lst[sub_item_index] > temp_list[0]:
+#                 result_list += get_sub_list(sub_item_index, init_lst, [temp_list[0]])    
+#     return result_list
+# Рабочий вариант
 # init_lst = [1, 5, 2, 3, 4, 6, 1, 7]
 # result_list = []
+# init_len = len(init_lst)
 
-# for i in range(len(init_lst)):
-#     j = i + 1
-#     temp_list = [init_lst[i]]
-#     while True:
-#         if j == len(init_lst): break
-#         if temp_list[len(temp_list) - 1] < init_lst[j]:
-#             temp_list += [init_lst[j]]
-#             j +=1
-#         else:
-#             break
-#     result_list += [temp_list]
+# def get_sub_list(item_index, init_lst, temp_list):
+#     result_list = []
+#     init_len = len(init_lst)
+#     for sub_item_index in range(item_index, init_len):
+#         if init_lst[sub_item_index] > temp_list[len(temp_list) - 1]:
+#             temp_list += [init_lst[sub_item_index]]
+#             result_list += [temp_list[:]]
+#     return result_list
+
+# for item_index in range(init_len):
+#     temp_list = [init_lst[item_index]] 
+#     result_list += [temp_list[:]]
+#     for sub_item_index in range(item_index + 1, init_len):
+#         result_list += get_sub_list(sub_item_index, init_lst, temp_list[:])
+
 # print(result_list)
+# print(len(result_list))
 
-init_lst = [1, 5, 2, 3, 4, 6, 1, 7]
-result_list = []
+# result_list = [tuple(item) for item in result_list]
+# print(set(result_list))
+# print(len(set(result_list)))
 
-for i in range(len(init_lst)):
-    j = i + 1
-    temp_list = [init_lst[i]]
-    new_init_list = init_lst[:]
-    while True:        
-        if j == len(new_init_list): break
-        if temp_list[len(temp_list) - 1] < new_init_list[j]:
-            temp_list += [new_init_list[j]]
-            j +=1
-        else:
-            result_list += [temp_list]
-            new_init_list.pop(j-1)
-            temp_list = [init_lst[i]]
-            j = i + 1
-            
-    result_list += [temp_list]
-print(len(result_list))
 
 # 57.	Дан список чисел. Выделить среди них максимальное количество чисел, удовлетворяющих условию предыдущей задачи. 
 # Пример: [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3, 4, 6, 7]
+# init_lst = [1, 5, 2, 3, 4, 6, 1, 7]
+# result_list = []
+# init_len = len(init_lst)
+
+# for item_index in range(init_len):
+#     temp_list = [init_lst[item_index]] 
+#     result_list += [temp_list[:]]
+#     for sub_item_index in range(item_index + 1, init_len):
+#         result_list += get_sub_list(sub_item_index, init_lst, temp_list[:])
+
+# print(result_list)
+# print(len(result_list))   
+# max_list = 0
+
+# save_i_list = []
+# for i_list in result_list:
+#     if len(i_list) > max_list:
+#         max_list = len(i_list)
+#         save_i_list = i_list
+# print(save_i_list)        
+
+# print(max(map(lambda x: len(x), [i_list for i_list in result_list])))
 
 # 58.	Напишите программу, удаляющую из текста все слова содержащие "абв".
-# str = 'ываабв лповап абвцукв алоабвабв'
+# str = 'ываабв лповап абвцукв алоабвабв ываываыв'
 # print(str)
-# str = str.split('абв')
-# str = reduce(lambda x, y: x + y, [i for i in str])
-# print(str)
+# tr = str.split()
+# [tr.remove(word) for word in tr[:] if 'абв' in word]        
+# print(tr)
+
 
 # 59.	Помните игру с конфетами из модуля "Математика и Информатика"? Создайте такую игру для игры человек против человека
+# https://cloud.mail.ru/public/kczH/yLqakTmUy
+sweets = 121
+max_sweets = 28
+turn = 1
+def get_hint_user(sweets, max_sweets):
+    if max_sweets >= sweets:
+        return sweets
+    number = sweets // max_sweets
+    y = number * max_sweets + 1
+    return sweets - y
+
+def get_hint_comp(sweets, max_sweets):
+    if max_sweets >= sweets:
+        return sweets
+    number = sweets // max_sweets    
+    y = number * max_sweets + 1
+    if y >= sweets:
+        y = (number - 1) * max_sweets + 1
+    return (random.randint(1, sweets if max_sweets > sweets else max_sweets) 
+    if sweets - y == 0 else sweets - y)
+
+def get_sweets(turn):
+    while True:
+        if turn == 1:
+            print(f'Заберите {get_hint_user(sweets, max_sweets)} конфет')
+            sweet_count = int(input(f'Сколько конфет забрать игроку {turn}:'))
+        else:
+            sweet_count = get_hint_comp(sweets, max_sweets)
+            print(f'Бот ввел число: {sweet_count}')
+        if sweet_count >= max_sweets:
+            sweet_count = max_sweets
+        return sweet_count    
+
+while True:
+    print(f'Конфет осталось {sweets}')
+    sweets -= get_sweets(turn)
+    if sweets <= 0:
+        turn = 'Бот' if turn == 2 else 'Человек'
+        print(f'Победил игрок {turn}')
+        break    
+    turn = 2 if turn == 1 else 1
+
 
 # a.	Добавьте игру против бота
 # b.	Подумайте как наделить бота "интеллектом" 
 # 60.	Вы когда-нибудь играли в игру "Крестики-нолики"? Попробуйте создать её.
+# def print_table(table_list):
+#     for index, row in enumerate(table_list, 1):
+#         print(row, end=' ')
+#         if not(index % 3): print('')
+        
+
+# def init_table():
+#     return [i for i in range(1, 10)]
+
+# def turn(lst, players_turn):    
+#     while True:
+#         cell = int(input('Введите ячейку для крестика: '))
+#         if not (lst[cell - 1] in ['O','X']): break
+#     lst[cell - 1] = players_turn 
+
+# def check_win(lst, players_turn):
+#     index_set = {index for index, value in enumerate(lst,1) if value == players_turn}
+#     for ch_set in win_list:
+#         if index_set == ch_set:
+#             return players_turn           
+#     return False
+
+# table_list = init_table()
+# win_list = [{1,2,3}, {4,5,6}, {7,8,9}, {1,5,9}, {3,5,7}, {1,4,7}, {2,5,8}, {3,6,9}]
+# players_turn = 'X'
+
+# for i in range(1, 10):    
+#     print_table(table_list)
+#     print(f'\nХод {i}')
+#     turn(table_list, players_turn)
+#     check = check_win(table_list, players_turn)
+#     if check != False:
+#         print(f'Победил {players_turn}')
+#         print_table(table_list)
+#         break
+#     players_turn = 'X' if players_turn == 'O' else 'O'
 # 61.	Написать программу вычисления арифметического выражения заданного строкой. Используются операции +,-,/,*. приоритет операций стандартный. Пример: 2+2 => 4; 1+2*3 => 7; 1-2*3 => -6; 
 # a.	Добавить возможность использования скобок, меняющих приоритет операций. Пример: 1+2*3 => 7; (1+2)*3 => 9;
 # 62.	Реализовать RLE алгоритм. реализовать модуль сжатия и восстановления данных.
 # a.	входные и выходные данные хранятся в отдельных файлах
+
+# tup = (254, 5, 45, 656)
+# print(*tup)
+
