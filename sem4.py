4d139be4f19c2e4f88b4b75c71ee13af36283b95
User <C:\Users\User\AppData\Roaming\The Bat!>
2022-02-09 23:41:34 +0300

    ex63

--- sem4.py
+++ sem4.py
@@ -432,3 +432,13 @@ from unittest import result
 # set_archive(text, 'archive.txt')
 # print(get_archive('archive.txt'))
 
+# 63.Дана последовательность чисел. Получить список уникальных элементов заданной последовательности.
+# Пример: [1, 2, 3, 5, 1, 5, 3, 10, 11] => [2, 10]
+
+lst =[1, 2, 3, 5, 1, 5, 3, 10, 11]
+dct = dict.fromkeys(lst, 0)
+print(dct)
+for item in lst:
+    dct[item] += 1
+
+print([index for index, value in dct.items() if value == 1])
